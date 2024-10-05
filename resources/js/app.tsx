8f68068a633a2ee4899813c19e2b36f69e85e2f8
ryfiii <mzk114.cube@gmail.com>
2024-10-05 23:53:48 +0900

    commit!

---
+++ resources/js/app.tsx
@@ -0,0 +1,15 @@
+import './bootstrap';
+
+import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = 'appName';
+
+createInertiaApp({
+    title: (title) => `${title}｜${appName}`,
+    resolve: name => resolvePageComponent(`./Pages/${name}.tsx`, import.meta.glob('./Pages/**/*.tsx')),
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title inertia>{{ config('app.name', 'appName') }}</title>
+
+    <!-- Scripts -->
+    @viteReactRefresh
+    @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
+    @inertiaHead
+</head>
+
+<body class="font-main text-base-content antialiased">
+    @inertia
+</body>
+
+</html>
